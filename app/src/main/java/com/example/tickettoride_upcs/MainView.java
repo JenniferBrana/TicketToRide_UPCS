6d703f640b0631f14fe2f1981dd6aad1521ce1b8
matsusht24 <matsusht24@up.edu>
2021-09-23 23:03:51 -0700

    Gui updtated

--- app/src/main/java/com/example/tickettoride_upcs/MainView.java
+++ app/src/main/java/com/example/tickettoride_upcs/MainView.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.provider.ContactsContract;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 
@@ -23,6 +25,11 @@ public class MainView extends SurfaceView {
     private Paint blackPaint;
     private Paint ticketPaint;
     private Paint textPaint;
+    private Paint ticketTextPaint;
+    private Paint trainCountPaint;
+    private Paint otherTrainCountPaint;
+    private Paint drawTrainPaint;
+    private Paint otherInfoPaint;
 
     public MainView(Context context, AttributeSet atr) {
         super(context, atr);
@@ -41,19 +48,39 @@ public class MainView extends SurfaceView {
         blackPaint = new Paint();
         ticketPaint =  new Paint();
         textPaint = new Paint();
+        ticketTextPaint = new Paint();
+        trainCountPaint = new Paint();
+        otherTrainCountPaint = new Paint();
+        drawTrainPaint = new Paint();
         white.setARGB(255,255,255,255);
         redPaint.setARGB(255,255,0,0);
         greenPaint.setARGB(255,0,255,0);
         bluePaint.setARGB(255,0,0,255);
-        yellowPaint.setARGB(255,255,255,0);
+        yellowPaint.setARGB(255,223,189,0);
         orangePaint.setARGB(255,255,165,0);
         greyPaint.setARGB(255,128,128,128);
         pinkPaint.setARGB(255, 255,192,203);
         blackPaint.setARGB(255,0 ,0,0);
         ticketPaint.setARGB(255,246,208,120);
         textPaint.setARGB(255,0 ,0,0);
+        ticketTextPaint.setARGB(255,0,0,0);
+        trainCountPaint.setARGB(255,0,0,0);
+        otherTrainCountPaint.setARGB(255,0,0,0);
+        drawTrainPaint.setARGB(255,173,216, 230);
         screenHeight = getMeasuredHeight();
         screenWidth = getMeasuredWidth();
+        ticketTextPaint.setTextSize(27);
+        textPaint.setTextSize(30);
+        trainCountPaint.setTextSize(50);
+        otherTrainCountPaint.setTextSize(25);
+        trainCountPaint.setFakeBoldText(true);
+        otherTrainCountPaint.setFakeBoldText(true);
+        textPaint.setFakeBoldText(true);
+        ticketTextPaint.setFakeBoldText(true);
+        otherInfoPaint = new Paint(otherTrainCountPaint);
+        otherInfoPaint.setARGB(255,255,255,255);
+
+
 
     }
 
@@ -75,77 +102,110 @@ public class MainView extends SurfaceView {
         //current player
         canvas.drawCircle( 2225, 1425,75,white);
         canvas.drawCircle( 2400, 1425,100,greenPaint);
+        canvas.drawText("44", 2190, 1450, trainCountPaint);
 
         //player 2
         canvas.drawCircle(600,100,70,redPaint);
-        canvas.drawRect(600, 70, 900, 170,redPaint);
-
+        canvas.drawRect(600, 60, 900, 170,redPaint);
+        canvas.drawCircle(660,140,27,white);
+        canvas.drawText("44", 645, 150, otherTrainCountPaint);
+        canvas.drawRect(720,70, 770, 110, drawTrainPaint);
+        canvas.drawText("3", 790, 100, otherInfoPaint);
+        canvas.drawRect(720,120, 770, 160, ticketPaint);
+        canvas.drawText("3", 790, 150, otherInfoPaint);
         //player 3
         canvas.drawCircle(  1025,100,70,bluePaint);
-        canvas.drawRect(1025, 70, 1325, 170,bluePaint);
+        canvas.drawRect(1025, 60, 1325, 170,bluePaint);
+        canvas.drawCircle(1085,140,27,white);
+        canvas.drawText("45", 1070, 150, otherTrainCountPaint);
+        canvas.drawRect(1145,70, 1195, 110, drawTrainPaint);
+        canvas.drawText("3", 1215, 100, otherInfoPaint);
+        canvas.drawRect(1145,120, 1195, 160, ticketPaint);
+        canvas.drawText("3", 1215, 150, otherInfoPaint);
 
         //player 4
         canvas.drawCircle(1450,100,70,yellowPaint);
-        canvas.drawRect(1450, 70, 1750, 170,yellowPaint);
+        canvas.drawRect(1450, 60, 1750, 170,yellowPaint);
+        canvas.drawCircle(1510,140,27,white);
+        canvas.drawText("41", 1495, 150, otherTrainCountPaint);
+        canvas.drawRect(1570,70, 1620, 110, drawTrainPaint);
+        canvas.drawText("3", 1640, 100, otherInfoPaint);
+        canvas.drawRect(1570,120, 1620, 160, ticketPaint);
+        canvas.drawText("3", 1640, 150, otherInfoPaint);
 
         /**
          * Board drawings
          *
          *
-         * Cities Dots
+         * Cities Dots and their names
          */
 
         //Astoria
         canvas.drawCircle(400,120,25,white);
+        canvas.drawText("Astoria", 270, 130, textPaint);
 
         //Tillamook
         canvas.drawCircle(390, 300, 25, white);
+        canvas.drawText("Tillamook", 235, 310,textPaint);
 
         //Newport
         canvas.drawCircle(360, 500, 25, white);
-
+        canvas.drawText("Newport", 220, 510, textPaint);
 
         //Coos Bay
         canvas.drawCircle(320, 880  , 25,white);
+        canvas.drawText("Coos Bay", 165, 890, textPaint);
 
         //Portland
         canvas.drawCircle(650,280, 25, white);
+        canvas.drawText("Portland", 660, 225, textPaint);
 
         //Salem
         canvas.drawCircle(600,420,25, white);
+        canvas.drawText("Salem", 640, 430, textPaint);
 
 
         //Eugene
         canvas.drawCircle(570,670,25,white);
+        canvas.drawText("Eugene", 600, 710, textPaint);
 
 
         //RoseBurg
         canvas.drawCircle(530,920,25,white);
+        canvas.drawText("RoseBurg", 560, 930, textPaint);
 
         //Grants Pass
         canvas.drawCircle(520,1125,25,white);
+        canvas.drawText("Grants Pass", 450, 1180, textPaint);
 
         //The Dalles
         canvas.drawCircle(1000,270, 25, white);
+        canvas.drawText("The Dalles", 980, 230, textPaint);
 
         //Bend
         canvas.drawCircle(1000,670, 25, white);
+        canvas.drawText("Bend", 1050, 670, textPaint);
 
         //K Falls
         canvas.drawCircle(880,1195,25, white);
+        canvas.drawText("K Falls", 920, 1160, textPaint);
 
         //Pendelton
         canvas.drawCircle(1560, 260, 25, white);
+        canvas.drawText("Pendleton", 1500, 230, textPaint);
 
         //La Grand
         canvas.drawCircle(1750, 350,25, white);
+        canvas.drawText("La Grand", 1780, 360, textPaint );
 
         //Burns
         canvas.drawCircle(1500, 820,25, white);
+        canvas.drawText("Burns", 1530, 830, textPaint);
 
 
         //LakeView
         canvas.drawCircle(1250,1195,25,white);
+        canvas.drawText("Lakeview",1280, 1205, textPaint);
 
 
            /**
@@ -155,7 +215,7 @@ public class MainView extends SurfaceView {
         canvas.save();
 
         //Astoria-Tillamook
-        canvas.drawRect(360,155, 390, 265, yellowPaint);
+        canvas.drawRect(360,155, 390, 265, white);
         canvas.drawRect(395,155, 425, 265, orangePaint);
 
         //Astoria-Portland
@@ -165,6 +225,7 @@ public class MainView extends SurfaceView {
         canvas.drawRect(390, -25,420,85,greyPaint);
         canvas.drawRect(390, -140,420,-30,greyPaint);
         canvas.restore();
+        //found how to rotate on stack overflow
 
         //Tillamook-Portland
         canvas.save();
@@ -186,10 +247,10 @@ public class MainView extends SurfaceView {
         //Portland-The Dalles
         canvas.save();
         canvas.rotate(89, 650,280);
-        canvas.drawRect(610,110, 640, 235,bluePaint );
-        canvas.drawRect(610,  -20, 640, 105,bluePaint );
-        canvas.drawRect(645,110, 675, 235,greenPaint );
-        canvas.drawRect(645,-20, 675, 105,greenPaint );
+        canvas.drawRect(610,110, 640, 235,orangePaint );
+        canvas.drawRect(610,  -20, 640, 105,orangePaint );
+        canvas.drawRect(645,110, 675, 235,pinkPaint );
+        canvas.drawRect(645,-20, 675, 105,pinkPaint );
         canvas.restore();
 
         //Portland-Salem
@@ -202,9 +263,9 @@ public class MainView extends SurfaceView {
         //The Dalles- Pendleton
         canvas.save();
         canvas.rotate(270,1000,270);
-        canvas.drawRect(965, 315, 995, 440, redPaint);
-        canvas.drawRect(965, 445, 995, 570, redPaint);
-        canvas.drawRect(965, 575, 995, 700, redPaint);
+        canvas.drawRect(965, 315, 995, 440, blackPaint);
+        canvas.drawRect(965, 445, 995, 570, blackPaint);
+        canvas.drawRect(965, 575, 995, 700, blackPaint);
         canvas.drawRect(1000, 315, 1030, 440, white);
         canvas.drawRect(1000, 445, 1030, 570, white);
         canvas.drawRect(1000, 575, 1030, 700, white);
@@ -221,10 +282,10 @@ public class MainView extends SurfaceView {
         //Pendleton-Bend
         canvas.save();
         canvas.rotate(53, 1560,260);
-        canvas.drawRect( 1545, 300, 1575, 440, bluePaint);
-        canvas.drawRect( 1545, 445, 1575, 585, bluePaint);
-        canvas.drawRect( 1545, 590, 1575, 730, bluePaint);
-        canvas.drawRect( 1545, 735, 1575, 875, bluePaint);
+        canvas.drawRect( 1545, 300, 1575, 440, pinkPaint);
+        canvas.drawRect( 1545, 445, 1575, 585, pinkPaint);
+        canvas.drawRect( 1545, 590, 1575, 730, pinkPaint);
+        canvas.drawRect( 1545, 735, 1575, 875, pinkPaint);
         canvas.drawRect( 1555, 300, 1565, 440, yellowPaint);
         canvas.drawRect( 1555, 445, 1565, 585, yellowPaint);
         canvas.drawRect( 1555, 590, 1565, 730, yellowPaint);
@@ -248,25 +309,25 @@ public class MainView extends SurfaceView {
         //Newport-CoosBoy
         canvas.save();
         canvas.rotate(7,360,500);
-        canvas.drawRect(345,535,375,635,bluePaint);
-        canvas.drawRect(345,640,375,740,bluePaint);
-        canvas.drawRect(345,745,375,845,bluePaint);
+        canvas.drawRect(345,535,375,635,pinkPaint);
+        canvas.drawRect(345,640,375,740,pinkPaint);
+        canvas.drawRect(345,745,375,845,pinkPaint);
         canvas.restore();
 
         //Newport-Eugene
         canvas.save();
         canvas.rotate(-52,360,500);
-        canvas.drawRect(345,535,375,630,greenPaint);
-        canvas.drawRect(345,635,375,730,greenPaint);
+        canvas.drawRect(345,535,375,630,white);
+        canvas.drawRect(345,635,375,730,white);
         canvas.restore();
 
         //Salem-Eugene
         canvas.save();
         canvas.rotate(5,600,420);
-        canvas.drawRect(565,455, 595,550, yellowPaint);
-        canvas.drawRect(565,555, 595,650, yellowPaint);
-        canvas.drawRect(600,455, 630,550, redPaint);
-        canvas.drawRect(600,555, 630,650, redPaint);
+        canvas.drawRect(565,455, 595,550,blackPaint);
+        canvas.drawRect(565,555, 595,650, blackPaint);
+        canvas.drawRect(600,455, 630,550, orangePaint);
+        canvas.drawRect(600,555, 630,650, orangePaint);
         canvas.restore();
 
         //Salem-Bend
@@ -280,17 +341,17 @@ public class MainView extends SurfaceView {
         //LaGrand-Burns
         canvas.save();
         canvas.rotate(28,1750,350);
-        canvas.drawRect(1735, 385, 1765, 495, yellowPaint);
-        canvas.drawRect(1735, 500, 1765, 610, yellowPaint);
-        canvas.drawRect(1735, 615, 1765, 725, yellowPaint);
-        canvas.drawRect(1735, 730, 1765, 840, yellowPaint);
+        canvas.drawRect(1735, 385, 1765, 495, orangePaint);
+        canvas.drawRect(1735, 500, 1765, 610, orangePaint);
+        canvas.drawRect(1735, 615, 1765, 725, orangePaint);
+        canvas.drawRect(1735, 730, 1765, 840, orangePaint);
         canvas.restore();
 
         //Euguene-Bend
         canvas.save();
         canvas.rotate(-89,570,670);
-        canvas.drawRect(555, 720, 585, 885,redPaint);
-        canvas.drawRect(555, 890, 585, 1050,redPaint);
+        canvas.drawRect(555, 720, 585, 885, pinkPaint);
+        canvas.drawRect(555, 890, 585, 1050,pinkPaint);
         canvas.restore();
 
         //Eugene-Roseburg
@@ -357,15 +418,15 @@ public class MainView extends SurfaceView {
         //Grants Pass-KFalls
         canvas.save();
         canvas.rotate(-77,520,1125);
-        canvas.drawRect(505,1170,535, 1295, redPaint);
-        canvas.drawRect(505,1300,535, 1425, redPaint);
+        canvas.drawRect(505,1170,535, 1295, orangePaint);
+        canvas.drawRect(505,1300,535, 1425, orangePaint);
         canvas.restore();
 
         //KFalls - LakeView
         canvas.save();
         canvas.rotate(-90, 880,1195);
-        canvas.drawRect(865,1230,895, 1365,greenPaint );
-        canvas.drawRect(865,1370,895, 1505,greenPaint );
+        canvas.drawRect(865,1230,895, 1365,white );
+        canvas.drawRect(865,1370,895, 1505,white );
         canvas.restore();
 
 
@@ -376,11 +437,11 @@ public class MainView extends SurfaceView {
         canvas.drawRect(10,1360,275 ,1430, ticketPaint);
         canvas.drawRect(0, 1440, 285, 1530, blackPaint);
         canvas.drawRect(10,1450,275 ,1520, ticketPaint);
-        //found from stack overflow
-        textPaint.setTextSize(27);
-        canvas.drawText("Portland-Padleton-7", 20.f,1310.f,textPaint);
-        canvas.drawText("Salem-Burns-12", 20.f,1390.f,textPaint);
-        canvas.drawText("Astoria-Lakeview-20", 20.f,1480.f,textPaint);
+
+
+        canvas.drawText("Portland-Padleton-7", 20.f,1310.f,ticketTextPaint);
+        canvas.drawText("Salem-Burns-12", 20.f,1390.f,ticketTextPaint);
+        canvas.drawText("Astoria-Lakeview-20", 20.f,1480.f,ticketTextPaint);
 
 
 
